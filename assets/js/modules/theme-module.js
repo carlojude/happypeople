a058a87ad39d93343a29bd289bdd0c64b4b405ff
Carlo Jude Alfonso <carlojude22@gmail.com>
2017-09-28 00:19:33 +0800

    Added Fonts and Fixed Scroll Nav

--- assets/js/modules/theme-module.js
+++ assets/js/modules/theme-module.js
@@ -99,7 +99,7 @@ AppName.Modules.ThemeModule = (function() {
             target = target.length ? target : $('[name=' + this.hash.substr(1) + ']');
             if (target.length) {
                 $('html,body').animate({
-                    scrollTop: target.offset().top
+                    scrollTop: target.offset().top - 120
                 }, 1000);
                 return false;
             }
